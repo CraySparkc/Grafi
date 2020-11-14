dd0cb9239c7c5949d69945a6c4cb340991fc93fe
Denis <dany.sssok@icloud.com>
2020-11-14 15:47:56 +0300

    111

--- appname/urls.py
+++ appname/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from appname import views
+from django.contrib.auth import views as authViews
 
 urlpatterns = [
-    #path('', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('login', views.user_login, name='login'),
+    path('exit', views.log_out, name='exit')
 ]
--- appname/views.py
+++ appname/views.py
@@ -1,6 +1,61 @@
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
-# Create your views here.
+from appname.form import UserProfileForm, UserForm
 
-#def index(request):
-   # return
+
+def index(request):
+    return render(request, 'index.html', {'values': [['Понедельник', 40, 10, 39], ['Вторник', 10, 29, 5], ['Среда', 96, 39, 20], ['Четверг', 10, 46, 13]]})
+
+
+'''def register(request):
+    registered = False
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileForm(data=request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+            profile = profile_form.save(commit=False)
+            profile.user = user
+
+            profile.save()
+            registered = True
+        else:
+            print(user_form.errors, profile_form.errors)
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+
+    return render(request,
+                  'registration/login.html',
+                  {'user_form': user_form,
+                   'profile_form': profile_form,
+                   'registered': registered})'''
+
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                return HttpResponse("Your appname account is disabled.")
+        else:
+            print("Invalid login details: {0}, {1}".format(username, password))
+            return HttpResponse("Invalid login details supplied.")
+    else:
+        return render(request, 'registration/login.html', {})
+
+
+def log_out(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
